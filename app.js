2a40a12efe8227a9172d0ba38cc42fd38bb2cdc0
meatbot <meatbot@pop-os.localdomain>
2022-09-05 16:13:49 +0900

    corrss

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ const { PORT = 3001, DB_LINK, NODE_ENV } = process.env;
 const allowedCors = [
   'http://localhost:3000',
   'http://localhost:3001',
+  'http://andrewdiploma.nomoredomains.xyz.nomoredomains.sbs',
+  'https://andrewdiploma.nomoredomains.xyz.nomoredomains.sbs',
+  'http://api.andrewdiploma.nomoredomains.xyz',
+  'https://api.andrewdiploma.nomoredomains.xyz',
 ];
 
 express.use(cors({ origin: allowedCors, credentials: true }));
